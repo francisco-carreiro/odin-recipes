046bd721dd5d4356631604665d73ce88b869b40c
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-08-24 18:06:41 -0100

    modal implemented

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("#MybtnModal").click(function () {
+    $("#Mymodal").modal("show");
+  });
+});
+
+/* const myModal = document.getElementById("myModal");
+const myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
+ */
